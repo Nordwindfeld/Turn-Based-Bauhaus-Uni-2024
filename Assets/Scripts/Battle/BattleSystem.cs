e90378815d871ef480048c78bdfc33d26858095f
Raphael Hürler <raphaelhuerler@yahoo.de>
2024-04-21 10:32:55 +0200

    Initial commit of my Unity project

---
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -0,0 +1,196 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYRTURN, WON, LOST }
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerPosition;
+    public Transform enemyPosition;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public Text EnemyNameText;
+    public Text EnemyLevelText;
+
+    public Text PlayerNameText;
+    public Text PlayerLevelText;
+    public Text PlayerTPCurrentText;
+    public Text PlayerTPMaxText;
+    public Text PlayerCurrentHealthText;
+    public Text PlayerMaxHealthText;
+    public Slider PlayerHPSlider;
+    public Slider PlayerTPSlider;
+    public Slider EnemyHPSlider;
+
+    public BattleState state;
+
+    public GameObject BattleMenu;
+
+    int LevelDamage;
+    int RandomDamage;
+    int PlayerDamage;
+    int EnemyDamage;
+
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+        BattleMenu.SetActive(false);
+    }
+
+    IEnumerator SetupBattle()
+    {
+        GameObject playerVariables = Instantiate(playerPrefab, playerPosition);
+        playerUnit = playerVariables.GetComponent<Unit>();
+
+        GameObject enemyVariables = Instantiate(enemyPrefab, enemyPosition);
+        enemyUnit = enemyVariables.GetComponent<Unit>();
+
+        EnemyNameText.text = enemyUnit.unitName;
+        EnemyLevelText.text = enemyUnit.unitLevel.ToString("D2");
+
+        PlayerNameText.text = playerUnit.unitName;
+        PlayerLevelText.text = playerUnit.unitLevel.ToString("D2");
+        PlayerTPCurrentText.text = playerUnit.currentTP.ToString("D3");
+        PlayerTPMaxText.text = playerUnit.maxTP.ToString("D3");
+        PlayerCurrentHealthText.text = playerUnit.currentHP.ToString("D3");
+        PlayerMaxHealthText.text = playerUnit.maxHP.ToString("D3");
+        PlayerHPSlider.maxValue = playerUnit.maxHP;
+        PlayerHPSlider.value = playerUnit.currentHP;
+        PlayerTPSlider.maxValue = playerUnit.maxTP;
+        PlayerTPSlider.value = playerUnit.currentTP;
+        EnemyHPSlider.maxValue = enemyUnit.maxHP;
+        EnemyHPSlider.value = enemyUnit.currentHP;
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    void SetUpBattleMenu()
+    {
+        BattleMenu.transform.position = playerPosition.position + new Vector3(775, 390, 0);
+        BattleMenu.SetActive(true);
+    }
+
+    public void CloseBattleMenu()
+    {
+        BattleMenu.SetActive(false);
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        CloseBattleMenu();
+        yield return PerformAttack(playerUnit, enemyUnit, EnemyHPSlider);
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        yield return PerformAttack(enemyUnit, playerUnit, PlayerHPSlider);
+    }
+
+    private IEnumerator PerformAttack(Unit attacker, Unit defender, Slider defenderHPSlider)
+    {
+        yield return new WaitForSeconds(2f);
+
+        int standardAttackDamage = attacker.attack;
+        int levelDamage;
+        int randomDamage = UnityEngine.Random.Range(-1, 3);
+        int critMultiplier = UnityEngine.Random.Range(1, 101) <= attacker.CritChance ? UnityEngine.Random.Range(1, 4) : 1;
+        int defenseEffect = (int)(defender.defense * 0.2);
+
+        float typeMultiplier = Unit.GetDamageMultiplier(attacker.type, defender.type);
+
+        if (attacker.unitLevel > defender.unitLevel)
+        {
+            levelDamage = (int)(Math.Abs(attacker.unitLevel - defender.unitLevel) * 0.5);
+        }
+        else if (attacker.unitLevel < defender.unitLevel)
+        {
+            levelDamage = (int)(Math.Abs(attacker.unitLevel - defender.unitLevel) * -0.4);
+        }
+        else
+        {
+            levelDamage = 0;
+        }
+
+        int rawDamage = standardAttackDamage + levelDamage + randomDamage - defenseEffect;
+        int effectiveDamage = Mathf.Max(0, rawDamage);
+        int totalDamage = Mathf.FloorToInt(effectiveDamage * critMultiplier * typeMultiplier);
+
+        bool isDead = defender.TakeDamage(totalDamage);
+
+        Debug.Log($"{attacker.unitName} attacks {defender.unitName} with total damage: {totalDamage} (Type multiplier: {typeMultiplier})");
+
+        defenderHPSlider.value = defender.currentHP;
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = (defender == playerUnit) ? BattleState.LOST : BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = (defender == playerUnit) ? BattleState.PLAYERTURN : BattleState.ENEMYRTURN;
+            if (state == BattleState.PLAYERTURN)
+            {
+                PlayerTurn();
+            }
+            else
+            {
+                StartCoroutine(EnemyTurn());
+            }
+        }
+    }
+
+
+    void EndBattle()
+    {
+        if(state == BattleState.WON)
+        {
+            // Programmieren was dann passieren soll.
+        } 
+        else if (state == BattleState.LOST)
+        {
+            // Programmieren bei Lose
+        }
+    }
+
+    void PlayerTurn()
+    {
+        SetUpBattleMenu();
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN){
+            return;
+        }
+        StartCoroutine(PlayerAttack());
+    }
+
+    public void OnSkillButton()
+    { 
+    
+    }
+
+    public void OnComboButton()
+    {
+        
+    }
+
+    public void OnItemButton()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Battle/Unit.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int unitLevel;
+
+    public int maxHP;
+    public int currentHP;
+
+    public int maxTP;
+    public int currentTP;
+
+    public UnitType type;
+
+    public int CritChance;
+    public int attack;
+    public int defense;
+
+    public enum UnitType
+    {
+        Fire,
+        Water,
+        Electric,
+        Techno,
+        Normal,
+        Dark,
+        Light,
+        Magic
+    }
+
+    private static Dictionary<(UnitType, UnitType), float> typeEffectiveness = new Dictionary<(UnitType, UnitType), float>()
+{
+    {(UnitType.Electric, UnitType.Water), 1.5f},
+    {(UnitType.Electric, UnitType.Techno), 1.2f},
+    {(UnitType.Electric, UnitType.Dark), 0.5f},
+    {(UnitType.Electric, UnitType.Electric), 0.5f},
+    {(UnitType.Electric, UnitType.Fire), 1.0f},
+    {(UnitType.Electric, UnitType.Normal), 1.0f},
+    {(UnitType.Electric, UnitType.Light), 1.0f},
+    {(UnitType.Electric, UnitType.Magic), 1.0f},
+
+    {(UnitType.Fire, UnitType.Water), 0.5f},
+    {(UnitType.Fire, UnitType.Techno), 1.0f},
+    {(UnitType.Fire, UnitType.Electric), 1.0f},
+    {(UnitType.Fire, UnitType.Normal), 1.0f},
+    {(UnitType.Fire, UnitType.Dark), 1.0f},
+    {(UnitType.Fire, UnitType.Light), 1.0f},
+    {(UnitType.Fire, UnitType.Magic), 1.5f},
+
+    {(UnitType.Water, UnitType.Fire), 1.5f},
+    {(UnitType.Water, UnitType.Techno), 1.0f},
+    {(UnitType.Water, UnitType.Electric), 0.5f},
+    {(UnitType.Water, UnitType.Normal), 1.0f},
+    {(UnitType.Water, UnitType.Dark), 1.0f},
+    {(UnitType.Water, UnitType.Light), 1.0f},
+    {(UnitType.Water, UnitType.Magic), 1.0f},
+
+    {(UnitType.Techno, UnitType.Water), 1.0f},
+    {(UnitType.Techno, UnitType.Fire), 1.0f},
+    {(UnitType.Techno, UnitType.Electric), 0.8f},
+    {(UnitType.Techno, UnitType.Normal), 1.5f},
+    {(UnitType.Techno, UnitType.Dark), 1.5f},
+    {(UnitType.Techno, UnitType.Light), 0.5f},
+    {(UnitType.Techno, UnitType.Magic), 0.5f},
+
+    {(UnitType.Normal, UnitType.Water), 1.0f},
+    {(UnitType.Normal, UnitType.Fire), 1.0f},
+    {(UnitType.Normal, UnitType.Electric), 1.0f},
+    {(UnitType.Normal, UnitType.Techno), 0.5f},
+    {(UnitType.Normal, UnitType.Dark), 1.0f},
+    {(UnitType.Normal, UnitType.Light), 1.0f},
+    {(UnitType.Normal, UnitType.Magic), 1.0f},
+
+    {(UnitType.Dark, UnitType.Water), 1.0f},
+    {(UnitType.Dark, UnitType.Fire), 1.0f},
+    {(UnitType.Dark, UnitType.Electric), 1.5f},
+    {(UnitType.Dark, UnitType.Techno), 0.5f},
+    {(UnitType.Dark, UnitType.Normal), 1.0f},
+    {(UnitType.Dark, UnitType.Light), 0.5f},
+    {(UnitType.Dark, UnitType.Magic), 1.5f},
+
+    {(UnitType.Light, UnitType.Water), 1.0f},
+    {(UnitType.Light, UnitType.Fire), 1.0f},
+    {(UnitType.Light, UnitType.Electric), 1.0f},
+    {(UnitType.Light, UnitType.Techno), 1.5f},
+    {(UnitType.Light, UnitType.Normal), 1.0f},
+    {(UnitType.Light, UnitType.Dark), 1.5f},
+    {(UnitType.Light, UnitType.Magic), 0.5f},
+
+    {(UnitType.Magic, UnitType.Water), 1.0f},
+    {(UnitType.Magic, UnitType.Fire), 0.5f},
+    {(UnitType.Magic, UnitType.Electric), 1.0f},
+    {(UnitType.Magic, UnitType.Techno), 1.5f},
+    {(UnitType.Magic, UnitType.Normal), 1.0f},
+    {(UnitType.Magic, UnitType.Dark), 0.5f},
+    {(UnitType.Magic, UnitType.Light), 1.5f},
+};
+
+
+
+    public bool TakeDamage(int damage)
+    {
+        currentHP -= damage;
+
+        if(currentHP <= 0) { 
+            return true;
+        }
+        else { return false; }
+    }
+
+    public static float GetDamageMultiplier(UnitType attackerType, UnitType defenderType)
+    {
+        return typeEffectiveness.TryGetValue((attackerType, defenderType), out float multiplier) ? multiplier : 1.0f;
+    }
+}
